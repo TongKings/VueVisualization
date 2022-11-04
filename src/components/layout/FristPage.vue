a0898f67fb31f676074706a2049d049046f0bff5
dazhou.tong@aitmed.com <3286614219@qq.com>
2022-11-04 13:54:36 +0800

    update

--- src/components/layout/FristPage.vue
+++ src/components/layout/FristPage.vue
@@ -1,31 +1,84 @@
 
 <template>
 
-    <div>
-        <canvas id="glCanvas" width="640" height="480">
-            <!-- 这里只有开启或者支持canvas的浏览器才不会显示 -->
-            你的浏览器似乎不支持或者禁用了 HTML5 <code>&lt;canvas&gt;</code> 元素。
-        </canvas>
+    <div id="glCanvas">
+
     </div>
 
 </template>
 
 <style scoped lang='scss'>
-        canvas {
+        #glCanvas {
             border: 1px solid;
+            // width: 400px;
+            // height: 400px;
         }
 </style>
 <script setup lang='ts'>
 import * as THREE from "three";
+import { onMounted } from "vue";
+
+onMounted(() => {
+    getThreeFun()
+})
+
 function getThreeFun(){
+    let div_target = document.getElementById("glCanvas");
+
     /**
      * 创建场景对象Scene
      */
     let scene = new THREE.Scene();
-    console.log(scene)
-    THREE
-    let geometry = new THREE.BoxGeometry(100,100,100);
+    /**
+     * 创建网格模型
+     */
+    let geometry = new THREE.SphereGeometry(60, 40, 40); //创建一个球体几何对象
+    // let geometry = new THREE.BoxGeometry(100, 100, 100); //创建一个立方体几何对象Geometry
+    let material = new THREE.MeshLambertMaterial({
+        color: 0x0000ff
+    }); //材质对象Material
+    let mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
+    scene.add(mesh); //网格模型添加到场景中
+    /**
+     * 光源设置
+     */
+    //点光源
+    let point = new THREE.PointLight(0xfff);
+    point.position.set(0, 0, 100); //点光源位置
+    scene.add(point); //点光源添加到场景中
+
+    /**
+     * 坐标轴设置
+     */
+    let axes = new THREE.AxesHelper(100);
+    scene.add(axes);
+    //环境光
+    let ambient = new THREE.AmbientLight(0x444444);
+    scene.add(ambient);
+    // console.log(scene)
+    // console.log(scene.children)
+    /**
+     * 相机设置
+     */
+    let width = window.innerWidth; //窗口宽度
+    let height = window.innerHeight; //窗口高度
+    let k = width / height; //窗口宽高比
+    let s = 100; //三维场景显示范围控制系数，系数越大，显示的范围越大
+    //创建相机对象
+    let camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+    camera.position.set(200, 300, 200); //设置相机位置
+    camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+    /**
+     * 创建渲染器对象
+     */
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);//设置渲染区域尺寸
+    renderer.setClearColor(0xb9d3ff, 1); //设置背景颜色
+    div_target?.appendChild(renderer.domElement); //body元素中插入canvas对象
+    //执行渲染操作   指定场景、相机作为参数
+    renderer.render(scene, camera)
+    console.log(scene,geometry,material,mesh,point,axes,camera,renderer)
 }
-getThreeFun()
+
 </script>
 
