20aeaa98ddd4f0faaa184d3768017ecde18ba083
dazhou.tong <dazhou.tong@aitmed.com>
2023-11-03 09:26:06 +0800

    asda

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import DataVVue3  from '@kjgl77/datav-vue3';
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'//添加elementplus图标
 const app = createApp(App)
-console.log(import.meta.env,"kkkkkkkk");
+// console.log(import.meta.env,"kkkkkkkk");
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)//每一个组件都添加图标
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,41 @@
 
 <script setup lang="ts">
-  import IndexView from '@/components/Index.vue'
+// import IndexView from '@/components/Index.vue'
+import Sphere from './SphereView.vue';
 
 </script>
 
 <template>
-       <IndexView msg="123"/>
-
+  <!-- <IndexView msg="123" /> -->
+  <div class="home">
+    <Sphere />
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  width: 100vw;
+  color: #fff;
+  background-color: #000;
+
+  ::-webkit-scrollbar {
+    /*滚动条整体*/
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-track {
+    /*滑轨*/
+    background-color: rgba(255, 255, 255, .2);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    /*滑块*/
+    background-color: rgba(255, 255, 255, .5);
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: rgba(255, 255, 255, 1);
+  }
 }
 </style>
